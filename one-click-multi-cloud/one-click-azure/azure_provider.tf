d21a05b69387e6a7a2d05d7814274bc9e0174ea6
vinayvenkat <vinayvenkat@yahoo.com>
2017-08-17 22:17:44 -0700

    Remove provider file

--- one-click-multi-cloud/one-click-azure/azure_provider.tf
+++
@@ -1,7 +0,0 @@
-
-provider "azurerm" {
-  subscription_id = "c3c31a6f-bc8d-4ae5-a783-0180bc5360cd"
-  client_id = "47c74120-9137-44ab-b209-1ec0f98b9080"
-  client_secret = "terraform123"
-  tenant_id = "41bb198b-a1f6-43a7-8370-7f0a446b7fba"
-}
