633552b6e6a5f34c6a3c354879b98f631c99f05c
vinayvenkat <vinayvenkat@yahoo.com>
2018-07-06 11:32:23 -0700

    Updated all the AMI id's

--- aws_two_tier/aws_vars.tf
+++ aws_two_tier/aws_vars.tf
@@ -11,20 +11,20 @@ variable "PANFWRegionMap" {
   type = "map"
   default =
     {
-      "us-west-2" = "ami-e614af86",
-      "ap-northeast-1" =   "ami-4bbcfa2c",
-      "us-west-1"      =   "ami-84a1fce4",
-      "ap-northeast-2" =   "ami-59419037",
-      "ap-southeast-1" =   "ami-17a41074",
-      "ap-southeast-2" =   "ami-10303673",
-      "eu-central-1"   =   "ami-e93df486",
-      "eu-west-1"      =   "ami-43f1aa25",
-      "eu-west-2"      =   "ami-d44d58b0",
-      "sa-east-1"      =   "ami-12b4d07e",
-      "us-east-1"      =   "ami-2127dc37",
-      "us-east-2"      =   "ami-810d28e4",
-      "ca-central-1"   =   "ami-6ebd000a",
-      "ap-south-1"     =   "ami-556a1b3a"
+      "us-west-2" = "ami-d424b5ac",
+      "ap-northeast-1" =   "ami-57662d31",
+      "us-west-1"      =   "ami-a95b4fc9",
+      "ap-northeast-2" =   "ami-49bd1127",
+      "ap-southeast-1" =   "ami-27baeb5b",
+      "ap-southeast-2" =   "ami-00d61562",
+      "eu-central-1"   =   "ami-55bfd73a",
+      "eu-west-1"      =   "ami-a95b4fc9",
+      "eu-west-2"      =   "ami-876a8de0",
+      "sa-east-1"      =   "ami-9c0154f0",
+      "us-east-1"      =   "ami-a2fa3bdf",
+      "us-east-2"      =   "ami-11e1d774",
+      "ca-central-1"   =   "ami-64038400",
+      "ap-south-1"     =   "ami-e780d988"
     }
 }
 variable "WebServerRegionMap" {
